1a959474b60aee282e806c67d8b52c78b480628b
blazeka <blazekada@gmail.com>
2017-06-29 06:43:25 +0200

    Extend repeat test to float32 dtype
    Avoid extensive tf.Session() creation

--- cxflow_tf/tests/utils_test.py
+++ cxflow_tf/tests/utils_test.py
@@ -15,26 +15,19 @@ class UtilsTest(CXTestCaseWithDir):
     """
 
     def test_repeat(self):
-        """ Test if `repeat` works."""
+        """ Test if `repeat` works the same as np.repeat."""
 
-        # try different tensor types
-        for init_value in [np.array([0, 1, 2, 3], dtype=np.int32),
-                           np.array([[0, 1], [2, 3], [4, 5]], dtype=np.int32)]:
-            # and all their axes
-            for axis in range(len(init_value.shape)):
-                # for some repeats (3 is often problematic)
-                for repeats in [1, 2, 3, 11]:
-                    tensor = tf.constant(init_value, dtype=tf.int32)
-                    repeated_tensor = repeat(tensor, repeats=repeats, axis=axis)
+        with tf.Session().as_default():
+            # try different tensor types
+            for npdtype, tfdtype in [(np.int32, tf.int32), (np.float32, tf.float32)]:
+                for init_value in [np.array([0, 1, 2, 3], dtype=npdtype),
+                                   np.array([[0, 1], [2, 3], [4, 5]], dtype=npdtype)]:
+                    # and all their axes
+                    for axis in range(len(init_value.shape)):
+                        for repeats in [1, 2, 3, 11]:
+                            tensor = tf.constant(init_value, dtype=tfdtype)
 
-                    sess = tf.Session()
-                    repeated_value = sess.run([repeated_tensor])
-                    expected_value = np.repeat(init_value, repeats=repeats, axis=axis)
+                            repeated_value = repeat(tensor, repeats=repeats, axis=axis).eval()
+                            expected_value = np.repeat(init_value, repeats=repeats, axis=axis)
 
-                    # TODO: this must be deleted before merge
-                    print(repeated_value)
-                    print(expected_value)
-                    print('OK' if np.all(repeated_value == expected_value) else 'Nope')
-                    print('----')
-
-                    self.assertTrue(np.all(repeated_value == expected_value))
+                            self.assertTrue(np.all(repeated_value == expected_value))
