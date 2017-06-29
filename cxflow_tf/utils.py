3d0e73c28847dde3290f1f1cbf1e7d5ef879837a
blazeka <blazekada@gmail.com>
2017-06-29 06:41:43 +0200

    Use more numerically stable way to infer repeat indices

--- cxflow_tf/utils.py
+++ cxflow_tf/utils.py
@@ -62,7 +62,7 @@ def repeat(tensor: tf.Tensor, repeats: int, axis: int) -> tf.Tensor:
     prepare_perm = np.hstack(([axis], np.delete(dims, axis)))
     restore_perm = np.hstack((dims[1:axis+1], [0], dims[axis+1:]))
 
-    indices = tf.cast(tf.floor(tf.range(0, shape[axis], 1.0/repeats)), 'int32')
+    indices = tf.cast(tf.floor(tf.range(0, shape[axis]*repeats)/tf.constant(repeats)), 'int32')
 
     shuffled = tf.transpose(tensor, prepare_perm)
     repeated = tf.gather(shuffled, indices)
