8f8b807773b1547f9277e6b9c2030e09c27428c3
Petr Belohlavek <me@petrbel.cz>
2017-08-22 09:12:22 +0200

    Remove `create_object_from_config`

--- cxflow_tensorflow/__init__.py
+++ cxflow_tensorflow/__init__.py
@@ -1,4 +1,4 @@
 from .model import BaseModel
-from .utils import TF_OPTIMIZERS_MODULE, create_activation, create_optimizer, repeat
+from .utils import create_activation, create_optimizer, repeat
 from .hooks import DecayLR, WriteTensorboard
 from .metrics import bin_dice, bin_stats
