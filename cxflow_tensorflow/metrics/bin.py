904396f9c3447997201836eba85ecf1006caa308
Petr Belohlavek <me@petrbel.cz>
2018-02-22 14:52:36 +0100

    Bin. stats name prefix

--- cxflow_tensorflow/metrics/bin.py
+++ cxflow_tensorflow/metrics/bin.py
@@ -6,21 +6,30 @@ from typing import Tuple, Optional
 import tensorflow as tf
 
 
-def bin_stats(predictions: tf.Tensor, labels: tf.Tensor, suffix: Optional[str]=None) -> Tuple[tf.Tensor, tf.Tensor,
-                                                                                              tf.Tensor]:
+def bin_stats(predictions: tf.Tensor, labels: tf.Tensor, prefix: Optional[str]=None,
+              suffix: Optional[str]=None) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
     """
     Calculate f1, precision and recall from binary classification expected and predicted values.
 
     :param predictions: 2-d tensor (batch, predictions) of predicted 0/1 classes
     :param labels: 2-d tensor (batch, labels) of expected 0/1 classes
+    :param prefix: prefix of the output tensor names
+    :param suffix: suffix of the output tensor names
     :return: a tuple of batched (f1, precision and recall) values
     """
-    # build correct suffix
+
+    # build correct prefix and suffix
     if suffix is None:
         suffix = ''
     else:
         suffix = '_' + suffix
 
+    if prefix is None:
+        prefix = ''
+    else:
+        prefix = prefix + '_'
+
+    # compute bin. statistics
     predictions = tf.cast(predictions, tf.int32)
     labels = tf.cast(labels, tf.int32)
 
@@ -29,11 +38,11 @@ def bin_stats(predictions: tf.Tensor, labels: tf.Tensor, suffix: Optional[str]=N
     false_negatives = tf.reduce_sum(tf.cast(tf.greater(labels, predictions), tf.int32), axis=1)
 
     recall = tf.identity(true_positives / (true_positives + false_negatives),
-                         name='recall'+suffix)
+                         name=prefix+'recall'+suffix)
     precision = tf.identity(true_positives / (true_positives + false_positives),
-                            name='precision' + suffix)
+                            name=prefix+'precision'+suffix)
     f1_score = tf.identity(2 / (1 / precision + 1 / recall),
-                           name='f1' + suffix)
+                           name=prefix+'f1'+suffix)
 
     return f1_score, precision, recall
 
