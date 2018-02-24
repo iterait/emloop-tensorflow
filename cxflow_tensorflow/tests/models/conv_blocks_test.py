2ecb54f2abd70960694e887b33ea07ca954303f2
blazeka <blazekada@gmail.com>
2018-02-24 18:39:33 +0100

    Add support for 3d convolution

--- cxflow_tensorflow/tests/models/conv_blocks_test.py
+++ cxflow_tensorflow/tests/models/conv_blocks_test.py
@@ -15,14 +15,14 @@ _VALID_CODES = {ResBlock: ['32res', '32ress2', '64ress3'],
                 MaxPoolBlock: ['mp2', 'mp3s2'],
                 UnPoolBlock: ['up2', 'up3'],
                 IncBlock: ['32inc', '64inc'],
-                ConvBlock: ['32c3', '64c5s2']}
+                ConvBlock: ['32c3', '64c5s2', '64c3-5s2', '63c3-3']}
 
 _INVALID_CODES = {ResBlock: ['32ress', 'res', '32res2', '32residual', 'res32', '32res 32res 32res', 'res32s0', '12inc'],
                   AveragePoolBlock: ['map2', 'up3s2', '3ap', 'mp', 'mp2 32c3', 'mp0', '3c', '24ress2', 'mp2', 'up3'],
                   MaxPoolBlock: ['map2', 'up3s2', '3ap', 'mp', 'mp2 32c3', 'mp0', '3c', '24ress2', 'ap2', 'up3'],
                   UnPoolBlock: ['map2', 'up3s2', '3ap', 'mp', 'mp2 32c3', 'mp0', '3c', '24ress2', 'mp2', 'ap3'],
                   IncBlock: ['32incs2', 'inc12', 'inception32', '64i', '54inc 54inc', '0inc', '32c'],
-                  ConvBlock: ['32c', '32cs2', 'c3', '64cs', '32c3 32c3', '0c3', '3c0', 'mp2']}
+                  ConvBlock: ['32c', '32cs2', 'c3', '64cs', '32c3 32c3', '0c3', '3c0', 'mp2', '32c3-3-3']}
 
 
 class BlocksTest(TestCase):
@@ -56,6 +56,8 @@ class BlocksTest(TestCase):
         dim5_kwargs = {'extra_dim': (1,), 'ap_fn': slim.avg_pool3d, 'mp_fn': slim.max_pool3d, 'bn_fn': slim.batch_norm,
                        'ln_fn': tf.contrib.layers.layer_norm, 'conv_fn': slim.conv3d, 'pool_fn': slim.max_pool3d}
 
+        only5dim = ['64c3-5s2', '63c3-3']
+
         block_ix = 0
         for shape, kwargs in (((3, 100, 100, 3), dim4_kwargs), ((3, 13, 100, 100, 3), dim5_kwargs)):
             with tf.Session() as ses:
@@ -64,10 +66,14 @@ class BlocksTest(TestCase):
                         block = block_type(code=code, **kwargs)
                         with tf.variable_scope('block_{}'.format(block_ix)):
                             x = tf.ones(shape)
-                            x = block.apply(x)
-                            ses.run(tf.local_variables_initializer())
-                            ses.run(tf.global_variables_initializer())
-                            value = x.eval(session=ses)
-                            self.assertIsInstance(value, np.ndarray)
-                            self.assertEqual(value.ndim, len(shape))
-                            block_ix += 1
+                            if len(shape) == 5 or code not in only5dim:
+                                x = block.apply(x)
+                                ses.run(tf.local_variables_initializer())
+                                ses.run(tf.global_variables_initializer())
+                                value = x.eval(session=ses)
+                                self.assertIsInstance(value, np.ndarray)
+                                self.assertEqual(value.ndim, len(shape))
+                                block_ix += 1
+                            else:
+                                with self.assertRaises(ValueError):
+                                    block.apply(x)
