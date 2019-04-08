2f9af7173a0f9509b1c4e04bd0c269a472d8006e
blazeka <blazekada@gmail.com>
2019-04-08 21:50:09 +0200

    Add get_coord_channels op

--- emloop_tensorflow/ops/__init__.py
+++ emloop_tensorflow/ops/__init__.py
@@ -6,5 +6,7 @@ from .flatten3d import flatten3D
 from .sparse import dense_to_sparse
 from .loss import smooth_l1_loss
 from .sequence import get_last_valid_features
+from .coord import get_coord_channels
 
-__all__ = ['repeat', 'flatten3D', 'dense_to_sparse', 'smooth_l1_loss', 'get_last_valid_features']
+__all__ = ['repeat', 'flatten3D', 'dense_to_sparse', 'smooth_l1_loss', 'get_last_valid_features',
+           'get_coord_channels']
